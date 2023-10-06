e8c54798871f7e016b5583c7ae529abe90dccdd9
nikoavgerinos <avgerinos.niko@gmail.com>
2023-10-06 20:39:26 +0200

    feat: Initialize pokemonList and add Pokémon data
    - removed existing code in "scripts.js."
    - created a pokemonList variable as an empty array.
    - added at least three Pokémon objects to pokemonList with "name," "height," and "types" properties.
    - used Pokedex.org as a reference for Pokémon data.
    - added code comments for documentation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
-alert('Hello world');
-
-var favoriteFood = "Ramen";
-
-document.write("My favorite food is: " + favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', 
+    height: 0.7, types: ["gras", "poison"]
+    },
+    {name: 'Gengar', 
+    height: 1.5, types: ["ghost", "poison"]
+    },
+    {name: 'Dragonite', 
+    height: 2.2, types: ["dragon", "flight"]
+    },
+];
