b5f38832b8f9c75855e7816af90a645e523f0a6e
nikoavgerinos <avgerinos.niko@gmail.com>
2023-10-10 15:11:14 +0200

    feat(script/html): Enhance Pokémon display and styling - (Task 1.3)
    In script.js, added a condition to display "Wow, that's big!" for Pokémon over 2 meters in height, along with displaying their height.
     Additionally, introduced necessary CSS classes for future styling.
     In index.html, added a <div> element with a class to serve as a Pokémon container, improving layout flexibility and maintainability.

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,21 @@ let pokemonList = [
     height: 2.2, types: ["dragon", "flight"]
     },
 ];
+
+// Initialize a loop with an index variable 'i' starting at 0
+for (let i = 0; i < pokemonList.length; i++) {
+    // Create a <div> element with the class "pokemon-info"
+    document.write('<div class="pokemon-info">');
+  
+    // Display the name and height of the current Pokémon
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m)");
+  
+    // Check if the current Pokémon is tall (height greater than 2.0)
+    if (pokemonList[i].height > 2.0) {
+      // Add a special message for tall Pokémon inside a <span> element
+      document.write('<span class="big-pokemon"> - Wow! That\'s big!</span>');
+    }
+  
+    // Close the <div> element
+    document.write('</div><br>');
+}
