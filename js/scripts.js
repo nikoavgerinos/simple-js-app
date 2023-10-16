13d747d1147c9e394ba112861c82fd0a777e727c
nikoavgerinos <avgerinos.niko@gmail.com>
2023-10-16 15:51:54 +0200

    refactor(scripts): implement forEach and IIFE in pokemonList management (Task 1.6)
    Refactor the 'scripts.js' to enhance list iteration and encapsulation:
    
    1. Replace the for loop with a forEach() method to iterate over the Pokémon in pokemonList, improving readability and simplifying the iteration process.
    
    2. Encapsulate the pokemonList within an Immediately Invoked Function Expression (IIFE) to prevent direct global state access, enhancing code stability and security.
    
    3. Introduce a pokemonRepository object that serves as an interface for interacting with the pokemonList, offering getAll() and add() functions.
    
    4. Adapt existing forEach loop to work with the new encapsulated pokemonList structure, ensuring functionality remains consistent with the previous implementation.
    
    5. Conduct thorough testing to ensure that the 'index.html' visual representation remains unchanged despite the refactor, confirming a seamless transition in code structure.
    
    Bonus:
    - Enhance the add() function within pokemonRepository to validate input, ensuring only objects with the correct structure are added to pokemonList, thereby maintaining data integrity.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,69 @@
-let pokemonList = [
-    {name: 'Bulbasaur', 
-    height: 0.7, types: ["gras", "poison"]
-    },
-    {name: 'Gengar', 
-    height: 1.5, types: ["ghost", "poison"]
-    },
-    {name: 'Dragonite', 
-    height: 2.2, types: ["dragon", "flight"]
-    },
-];
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Gengar', height: 1.5, types: ['ghost', 'poison']},
+    {name: 'Dragonite', height: 2.2, types: ['dragon', 'flying']}
+    // ... other Pokemon data
+  ];
 
-// Initialize a loop with an index variable 'i' starting at 0
-for (let i = 0; i < pokemonList.length; i++) {
-    // Create a <div> element with the class "pokemon-info"
-    document.write('<div class="pokemon-info">');
-  
-    // Display the name and height of the current Pokémon
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m)");
-  
-    // Check if the current Pokémon is tall (height greater than 2.0)
-    if (pokemonList[i].height > 2.0) {
-      // Add a special message for tall Pokémon inside a <span> element
-      document.write('<span class="big-pokemon"> - Wow! That\'s big!</span>');
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    if (typeof pokemon === 'object') {
+      let expectedKeys = ['name', 'height', 'types'];
+      let objectKeys = Object.keys(pokemon);
+      let allKeysExist = expectedKeys.every((key) => objectKeys.includes(key));
+
+      if (allKeysExist) {
+        pokemonList.push(pokemon);
+      } else {
+        console.error("The object doesn't have the correct structure for a Pokemon entity.");
+      }
+    } else {
+      console.error("You can only add objects to the Pokemon list.");
     }
-  
-    // Close the <div> element
-    document.write('</div><br>');
+  }
+
+  function findByName(name) {
+    return pokemonList.filter(function(pokemon) {
+      return pokemon.name.toLowerCase() === name.toLowerCase();
+    });
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    findByName: findByName
+  };
+})();
+
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: ["electric"] }); 
+
+
+let allPokemons = pokemonRepository.getAll();
+
+
+allPokemons.forEach(function(pokemon) {
+  console.log(pokemon); 
+});
+
+
+
+for (let i = 0; i < allPokemons.length; i++) {
+ 
+  document.write('<div class="pokemon-info">');
+
+
+  document.write(allPokemons[i].name + " (height: " + allPokemons[i].height + "m)");
+
+
+  if (allPokemons[i].height > 2.0) {
+
+    document.write('<span class="big-pokemon"> - Wow! That\'s big!</span>');
+  }
+
+
+  document.write('</div><br>');
 }
