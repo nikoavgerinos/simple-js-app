b058ab769f64f2b72f2d532b226afee4b00f26b3
nikoavgerinos <avgerinos.niko@gmail.com>
2023-10-19 17:34:16 +0200

    feat(scripts): integrate Pokemon external API and update polyfills (Task 1.7)
    This commit encompasses several updates to the application's functionality and compatibility support:
    
    1. Added 'promise-polyfill.js' to support Promise API in environments where it's not natively available. The corresponding script tag has been included in 'index.html'.
    
    2. Introduced 'fetch-polyfill.js' to ensure the Fetch API works across browsers that do not support it natively. The script tag for this has been integrated into 'index.html'.
    
    3. Replaced static Pokemon data with dynamic data retrieval from an external Pokemon API. This involved modifications to the 'addListItem()' and 'showDetails()' functions within 'scripts.js', ensuring real-time data accuracy and enhancing the application's data set.
    
    By integrating these polyfills, the application now has broader browser compatibility, and the shift to a dynamic data set enriches the user experience with up-to-date information.

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,88 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-    { name: 'Gengar', height: 1.5, types: ['ghost', 'poison'] },
-    { name: 'Dragonite', height: 2.2, types: ['dragon', 'flying'] }
-    // ... other Pokemon data
-  ];
-
-  function getAll() {
-    return pokemonList;
-  }
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     if (
       typeof pokemon === 'object' &&
       'name' in pokemon &&
-      'height' in pokemon &&
-      'types' in pokemon
+      'detailsUrl' in pokemon 
     ) {
       pokemonList.push(pokemon);
     } else {
-      console.error("pokemon is not correct format");
+      console.error("Pokemon is not in the correct format.");
     }
   }
 
+  function getAll() {
+    return pokemonList;
+  }
+
   function addListItem(pokemon) {
-    let pokemonList = document.querySelector('.pokemon-list');
+    let pokemonListElement = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('pokemon-button');
     listItem.appendChild(button);
-    pokemonList.appendChild(listItem);
-    // Adding the event listener for the button
+    pokemonListElement.appendChild(listItem);
     addEventListenerToButton(button, pokemon);
   }
 
-  // Creating a new function to handle the event listener addition
   function addEventListenerToButton(button, pokemon) {
     button.addEventListener('click', function () {
       showDetails(pokemon);
     });
   }
 
-  // Updated showDetails function to just log the pokemon object
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types.map(function (type) {
+        return type.type.name;
+      });
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon); 
+    });
   }
 
   return {
-    getAll: getAll,
     add: add,
-    addListItem: addListItem
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails 
   };
 })();
 
-// Adding a new Pokémon to demonstrate functionality
-pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: ['electric'] });
-
-// Iterating over each Pokémon in the repository and creating a list item for each
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
